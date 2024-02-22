e221513dcfd16210a806b9d8551783d7dac4dc7e
MiaMarmeladenbrot <mia.mecklenburg@gmx.net>
2024-02-22 15:04:16 +0100

    zwischenstand version 2

--- Version2/assets/js/main.js
+++ Version2/assets/js/main.js
@@ -1,107 +1,103 @@
-// ! VERSION 1
-// 1. HTML Formular bauen mit leerem div und Ausgabe-section (Felder einzeln?)
-// 2. Wetterdaten fetchen mit fester lat und long
-// 3. Daten ins HTML schreiben
+// ! VERSION 2
+// 1. HTML Formular bauen mit leeren Ausgabe-Feldern ✅
+// 2. output Variablen ✅
+// 3. eventListener, deshalb mit appendChild ✅
+// 4. value auslesen ✅
+// 5. Geodaten fetchen ✅
+// 6. Wetterdaten fetchen mit variablen lat und long
+// 7. Daten ins HTML schreiben
+// UND: Resultate reduzieren, select/option?
 
-// ! Output-Variable
+// ! Variablen
 const weatherDataOutput = document.querySelector(".weather-data-output");
 // console.log(weatherDataOutput);
-
 const mainInfoOutput = document.querySelector(".main-info");
 const moreInfoOutput = document.querySelector(".more-info");
+const errorMessage = document.querySelector(".error-message");
+const submitBtn = document.querySelector("#submit");
 
-// eventListener, deshalb mit appendChild
-// Value auslesen
+// ! Event-Listener fürs Eingeben der Stadt
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
 
-// ! API für Berlin fetchen
-fetch(
-  "https://api.openweathermap.org/data/2.5/weather?lat=52.520008&lon=13.404954&units=metric&lang=de&appid=4d391bfa015027f6dda47c22088a30a6"
-)
-  .then((res) => res.json())
-  .then((weatherData) => {
-    console.log(weatherData);
+  // * User-Input auslesen:
+  const userInput = document.querySelector("#city-input").value.toLowerCase();
+  // console.log(userInput);
 
-    // * Sunrise und Sunset in Uhrzeit umwandeln:
-    // leider sind die Daten für Sonnenaufgang falsch ...
-    const sunrise = weatherData.sys.sunrise;
-    const sunriseDatum = new Date(sunrise);
-    const sunriseTime = sunriseDatum.toLocaleTimeString("de-DE", {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-    console.log(sunriseTime);
+  // * Fehlermeldung, falls nichts eingegeben:
+  if (userInput.length === 0) {
+    errorMessage.innerHTML = `Bitte gib eine Stadt ein`;
+  }
 
-    const sunset = weatherData.sys.sunset;
-    const sunsetDatum = new Date(sunset);
-    const sunsetTime = sunsetDatum.toLocaleTimeString("de-DE", {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-    console.log(sunsetTime);
+  // * Geodaten fetchen mit User-Input:
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=10&lang=de&appid=4d391bfa015027f6dda47c22088a30a6`
+  )
+    .then((res) => res.json())
+    .then((geoData) => {
+      // console.log(geoData);
 
-    // * großes Hauptinfo-Feld betexten:
-    mainInfoOutput.innerHTML = `
-    <p>${new Date().toLocaleDateString()} | ${new Date().toLocaleTimeString(
-      "de-DE",
-      {
-        hour: "2-digit",
-        minute: "2-digit",
-      }
-    )}</p>
-    <h3>Berlin-${weatherData.name} (${weatherData.sys.country})</h3>
-    <img src="https://openweathermap.org/img/wn/${
-      weatherData.weather[0].icon
-    }@2x.png">
-    <h2>${Math.round(weatherData.main.temp)} °C</h2>
-    <p>gefühlt wie ${Math.round(weatherData.main.feels_like)} °C</p>
-    <p>${weatherData.weather[0].description}</p>
-    `;
+      // * über Geo-Daten des User-Inputs iterieren, um alle gefetchten Matches zum User-Input zu erhalten und zu durchlaufen:
+      geoData.forEach((item) => {
+        // console.log(item.lat);
+        // console.log(item.lon);
 
-    // * kleineres Feld mit weiteren Infos betexten, sofern sie im json vorhanden sind:
-    moreInfoOutput.innerHTML = `
-    ${
-      weatherData.rain
-        ? `<p>Niederschlagsintensität | ${weatherData.rain["1h"]} mm/h</p>`
-        : ""
-    }
-    ${
-      weatherData.wind.speed
-        ? `<p>Wind | ${Math.round(weatherData.wind.speed * 3.6)} km/h</p>`
-        : ""
-    }
-    ${
-      weatherData.wind.gust
-        ? `<p>Böen | ${Math.round(weatherData.wind.gust * 3.6)} km/h</p>`
-        : ""
-    }
+        // * Wetterdaten fetchen mit User-Input:
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${item.lat}&lon=${item.lon}&units=metric&lang=de&appid=4d391bfa015027f6dda47c22088a30a6`
+        )
+          .then((res) => res.json())
+          .then((weatherData) => {
+            console.log(weatherData);
 
-    ${
-      weatherData.main.humidity
-        ? `<p>Luftfeuchtigkeit | ${weatherData.main.humidity} %</p>
-          `
-        : ""
-    }
-    
-    <p>Sonnenaufgang | ${sunriseTime}</p>
-    <p>Sonnenuntergang | ${sunsetTime}</p>
+            // * Main-Info-Box betexten:
+            mainInfoOutput.innerHTML = `
+            <p>${new Date().toLocaleDateString()} | ${new Date().toLocaleTimeString(
+              "de-DE",
+              {
+                hour: "2-digit",
+                minute: "2-digit",
+              }
+            )}</p>
+            <h3>${weatherData.name} (${weatherData.sys.country})</h3>
+            <img src="https://openweathermap.org/img/wn/${
+              weatherData.weather[0].icon
+            }@2x.png">
+            <h2>${Math.round(weatherData.main.temp)} °C</h2>
+            <p>gefühlt wie ${Math.round(weatherData.main.feels_like)} °C</p>
+            <p>${weatherData.weather[0].description}</p>
+            `;
 
+            // * kleineres Feld mit weiteren Infos betexten, sofern sie im json vorhanden sind:
+            moreInfoOutput.innerHTML = `
+            ${
+              weatherData.rain
+                ? `<p>Niederschlagsintensität: ${weatherData.rain["1h"]} mm/h</p>`
+                : ""
+            }
+            ${
+              weatherData.wind.speed
+                ? `<p>Wind: ${Math.round(
+                    weatherData.wind.speed * 3.6
+                  )} km/h</p>`
+                : ""
+            }
+            ${
+              weatherData.wind.gust
+                ? `<p>Böen: ${Math.round(weatherData.wind.gust * 3.6)} km/h</p>`
+                : ""
+            }
 
-    `;
-  })
-  .catch((err) => console.log("geht nicht", err));
-
-// function checkData(data, des) {
-//   data ? `<p>${des} | ${data} mm/h</p>` : "";
-// }
-
-// ! gelöschte Teile
-{
-  /* <p>Wind | ${Math.round(weatherData.wind.speed * 3.6)} km/h</p> */
-}
-
-{
-  /* <p>Böen | ${Math.round(weatherData.wind.gust * 3.6)} km/h</p>
-    <p>Luftfeuchtigkeit | ${weatherData.main.humidity} %</p>
-    <p>Sonnenaufgang | ${sunriseTime}</p>
-    <p>Sonnenuntergang | ${sunsetTime}</p> */
-}
+            ${
+              weatherData.main.humidity
+                ? `<p>Luftfeuchtigkeit: ${weatherData.main.humidity} %</p>
+                  `
+                : ""
+            }
+            `;
+          })
+          .catch((err) => console.log("fehler beim wetterdaten fetchen", err));
+      });
+    })
+    .catch((err) => console.log("fehler beim geo fetch", err));
+});
